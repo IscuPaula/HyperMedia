aafb8d0060a0a1dd600d810fd5ed2446bf9dc199
PAULA-IULIANA ISCU <iscu.paula@gmail.com>
2021-10-26 14:26:59 +0300

    Day 2 commit

---
+++ application/views/home_view.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html lang='en'>
+    <head>
+        <title> Home </title>
+        <link rel = "stylesheet" type = "text/css"  href = "<?php echo base_url(); ?>css\home.css">  
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
+    <body>
+        hello
+    </body>
+</html> 
